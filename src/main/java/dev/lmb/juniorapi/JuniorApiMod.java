ba9a7f71b4420d3c5c0519a912af39c39d82b092
Lu <164914586+LucaTube@users.noreply.github.com>
2026-01-03 18:02:42 -0300

    Initial commit

---
+++ src/main/java/dev/lmb/juniorapi/JuniorApiMod.java
@@ -0,0 +1,10 @@
+package dev.lmb.juniorapi;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("juniorapi")
+public class JuniorApiMod {
+    public JuniorApiMod() {
+        // init vazio
+    }
+}
